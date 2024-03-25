57fedeb3ba0f532debd3a0e8fc4ca86f860f6666
ssayres <ssayres@outlook.com>
2024-03-24 23:31:18 -0300

    avanços em relação ao backend em java spring/ rest api

---
+++ cardapio/src/main/java/food/FoodResponseDTO.java
@@ -0,0 +1,9 @@
+package food;
+
+import jdk.jfr.RecordingState;
+
+public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+    public FoodResponseDTO(Food food){
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
